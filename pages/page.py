004b28cf1fcdc60ae587b2bbdfc9871f2b3bf19d
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2025-10-01 09:19:57 +0200

    refactor: stuff from pagea to page

--- pages/page.py
+++ pages/page.py
@@ -6,15 +6,25 @@ from config import HEIGHT
 class Page:
     def __init__(self, framebuffer: FrameBuffer):
         self.framebuffer = framebuffer
+        self.components: list = []
+        self.x: int = 0
+        self.y: int = HEIGHT // 2
 
 
     def initial(self):
-        pass
+        self.framebuffer.clear_buffer()
+
+        for component in self.components:
+            component.Initial()
 
 
     def Tick(self):
-        pass
+        self.framebuffer.clear_buffer()
+
+        for component in self.components:
+            component.Tick()
 
 
     def KeyPressed(self, keys: Sequence):
-        pass
+        for component in self.components:
+            component.KeyPressed(keys)
--- pages/page_a.py
+++ pages/page_a.py
@@ -13,9 +13,7 @@ from components.component_textfield import ComponentTextField
 class PageA(Page):
     def __init__(self, framebuffer: FrameBuffer):
         super().__init__(framebuffer)
-        self.components: list = []
-        self.x: int = 0
-        self.y: int = HEIGHT // 2
+
         self.dx: int = 1
 
         self.components.append(ComponentClock(framebuffer, 1, 0))
@@ -23,24 +21,3 @@ class PageA(Page):
         self.components.append(ComponentMover(framebuffer, 1, 1))
         self.components.append(ComponentTextField(framebuffer, 15, 2, 20))
         self.components.append(ComponentMenu(framebuffer, ["Item 1", "Item 2", "Item 3"], 1, 4))
-
-
-    def initial(self):
-        self.framebuffer.clear_buffer()
-
-        for component in self.components:
-            component.Initial()
-
-
-    def Tick(self):
-        self.framebuffer.clear_buffer()
-
-        for component in self.components:
-            component.Tick()
-
-
-    def KeyPressed(self, keys: Sequence):
-        for component in self.components:
-            component.KeyPressed(keys)
-
-            
