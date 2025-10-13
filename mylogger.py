c3fb237eeb24da24abd8fd471fab995a6e51530c
dobin <dobin.rutishauser@gmail.com>
2025-10-13 21:40:43 +0200

    feature: log to stdout in minitel mode

--- mylogger.py
+++ mylogger.py
@@ -1,13 +1,21 @@
 import time
 
+from config import *
 
 class MyLogger:
     def __init__(self):
         self.file = open("makrotel.log", "w")
 
     def log(self, message):
-        self.file.write(f"{time.ctime()}: {message}\n")
-        self.file.flush()
+        log = f"{time.ctime()}: {message}\n"
+        if PROG_MODE == "minitel":
+            print(log)
+        else:
+            self.file.write(log)
+            self.file.flush()
+
+    def set_mode(self, mode: str):
+        self.mode = mode
 
 myLogger = MyLogger()
 
